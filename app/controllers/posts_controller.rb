aff7fc578be2e0244988807bdfe3ca2a2fbf00cf
Luciekimotho <luciekimotho@gmail.com>
2015-09-18 14:25:58 +0300

    Styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
+
+	#display all blogs at the index fle
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
+    #to add new posts
 	def new 
 		@post = Post.new
 	end
+
 
 	def create
 		@post = Post.new(post_params)
@@ -17,10 +21,28 @@ class PostsController < ApplicationController
         end
 	end
 
+    #displaying a blog
 	def show 
 		@post = Post.find(params[:id])
 	end
 
+
+    #code to edit and update a post
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+		   redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+     #parameters for a post
 	private def post_params
 	   params.require(:post).permit(:title, :body)
 	end
